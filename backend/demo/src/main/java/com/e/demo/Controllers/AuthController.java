5d289b8c0ca63cd255d86673c50067f5f09f1672
Danial Bakirov <vasya@pupkin.ru>
2026-03-20 06:16:44 +0500

    auth

--- backend/demo/src/main/java/com/e/demo/Controllers/AuthController.java
+++ backend/demo/src/main/java/com/e/demo/Controllers/AuthController.java
@@ -6,6 +6,8 @@ import com.e.demo.entity.User;
 import com.e.demo.repository.UserRepository;
 import com.e.demo.server.JwtService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Log4j2
 public class AuthController {
 
     private final UserRepository userRepository;
@@ -25,6 +28,7 @@ public class AuthController {
             .orElseThrow(() -> new RuntimeException("Пользователь не найден"));
 
         if (!passwordEncoder.matches(req.password(), user.getPasswordHash())) {
+            log.info("Хеш пароль", req.password());
             return ResponseEntity.status(401).build();
         }
 
